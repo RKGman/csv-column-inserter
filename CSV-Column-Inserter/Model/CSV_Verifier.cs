76bc18b99ea7e5be8238ff2be2e61b9f339a5b63
RK <rg.pvndev@gmail.com>
2020-02-13 14:56:04 -0600

    Added packages for MVVM and updated MainViewModel for selecting a file.

---
+++ CSV-Column-Inserter/Model/CSV_Verifier.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualBasic.FileIO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSV_Column_Inserter.Model
+{
+    class CSV_Verifier
+    {
+        private string _filePath = "";
+        private TextFieldParser _parser = null;
+        public CSV_Verifier(string filePath) 
+        {
+            _filePath = filePath;
+        }
+
+        public bool IsValidCSV()
+        {
+
+        }
+    }
+}
--- CSV-Column-Inserter/ViewModel/MainViewModel.cs
+++ CSV-Column-Inserter/ViewModel/MainViewModel.cs
@@ -1,12 +1,67 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.Win32;
+using Prism.Commands;
+using Prism.Mvvm;
+using System;
+using System.ComponentModel;
+using System.Windows.Input;
 
 namespace CSV_Column_Inserter.ViewModel
 {
-    class MainViewModel
+    class MainViewModel : BindableBase, INotifyPropertyChanged
     {
+        private string _selectedFilePath = "Please selected a CSV file...";        
+        private DelegateCommand _browseFileCommand;
+        private bool _isValidCSV = false; 
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+        public ICommand BrowseFileCommand
+        {
+            get
+            {
+                if (_browseFileCommand == null)
+                {
+                    _browseFileCommand = new DelegateCommand(ExecuteBrowseFile);
+                }
+                return _browseFileCommand;
+            }
+        }
+        private void ExecuteBrowseFile()
+        {
+            OpenFileDialog openFD = new OpenFileDialog();
+            openFD.DefaultExt = ".csv";
+            openFD.Filter = "csv Files (*.csv)|*.csv|CSV Files (*.CSV)|*.CSV";
+            Nullable<bool> result = openFD.ShowDialog();
+
+            if (result.HasValue && result.Value)
+            {
+                // Set the selected file name
+                SelectedFilePath = openFD.FileName;
+            }
+        }
+
+        public bool IsValidCSV  
+        {
+            get { return _isValidCSV; }
+            set
+            {
+                _isValidCSV = value;
+                OnPropertyChanged("IsValidCSV");
+            }
+        }
+
+        public string SelectedFilePath
+        {
+            get { return _selectedFilePath; }
+            set
+            {
+                _selectedFilePath = value;
+                OnPropertyChanged("SelectedFilePath");
+            }
+        }
     }
 }
